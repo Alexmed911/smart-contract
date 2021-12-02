61e4810e01d925e233552b44fa331f9a8511a8b5
Alexmed911 <87994565+Alexmed911@users.noreply.github.com>
2021-12-02 14:01:15 +0200

    Update ERC20.sol

--- Smart contract/Java_lib/ERC20.sol
+++ Smart contract/Java_lib/ERC20.sol
@@ -1,10 +1,10 @@
 pragma solidity ^0.4.18;
 
 contract TokenERC20 {
-    string public name;
-    string public symbol;
+    string public Evmos-test;
+    string public Evmos ;
     uint8 public decimals;
-    uint256 public totalSupply;
+    uint256 public 100000000;
 
     mapping (address => uint256) public balanceOf;
     mapping (address => mapping (address => uint256)) public allowance;
